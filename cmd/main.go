2ac85fa014b202a45ab745b9c94d800649973d7f
akydyrba <abdulmalik.kydyrbay@nu.edu.kz>
2025-01-15 10:25:18 +0500

    postgres imported to main

--- cmd/main.go
+++ cmd/main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
+	"frappuccinno/postgres"
 	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
-const (
-	dbUser     = "latte"
-	dbPassword = "latte"
-	dbHost     = "db"
-	dbPort     = "5432"
-	dbName     = "frappuccino"
-)
+// const (
+// 	dbUser     = "latte"
+// 	dbPassword = "latte"
+// 	dbHost     = "db"
+// 	dbPort     = "5432"
+// 	dbName     = "frappuccino"
+// )
 
-func getDBConnectionString() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		dbUser, dbPassword, dbHost, dbPort, dbName)
-}
+// func getDBConnectionString() string {
+// 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+// 		dbUser, dbPassword, dbHost, dbPort, dbName)
+// }
 
 // id VARCHAR(255) PRIMARY KEY,
 // name VARCHAR(255) NOT NULL,
@@ -27,7 +28,7 @@ func getDBConnectionString() string {
 // phone VARCHAR(50),
 func main() {
 	// Connect to the database
-	connStr := getDBConnectionString()
+	connStr := postgres.getDBConnectionString()
 	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
